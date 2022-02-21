9d6ce6227b2db78590e8b5800995145975052950
Kevin Leung <kevinresol@gmail.com>
2022-02-21 15:22:24 +0800

    Add a rest api to unlink user's federation
    because it is currently not possible via the official admin rest api
    see: https://github.com/keycloak/keycloak/blob/febb447919044943241ad97200821c7da5099cb3/services/src/main/java/org/keycloak/services/resources/admin/UserResource.java#L234

--- src/haxe/com/beeinventor/keycloak/CognitoMigrationResourceProvider.hx
+++ src/haxe/com/beeinventor/keycloak/CognitoMigrationResourceProvider.hx
@@ -3,6 +3,7 @@ package com.beeinventor.keycloak;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.services.resource.RealmResourceProvider;
 
+// TODO: auth
 class CognitoMigrationResourceProvider implements RealmResourceProvider {
 	final session:KeycloakSession;
 	
@@ -31,6 +32,33 @@ class CognitoMigrationResourceProvider implements RealmResourceProvider {
 		
 		return haxe.Json.stringify({
 			result: user.isPresent() ? user.get().getId() : null,
+		});
+	}
+	
+	@:keep
+	@:meta(javax.ws.rs.POST())
+	@:meta(javax.ws.rs.Path(value = 'unlink'))
+	@:meta(javax.ws.rs.Produces(value = ['application/json']))
+	public function unlink(
+		@:meta(javax.ws.rs.QueryParam(value = 'realm')) realm:String,
+		@:meta(javax.ws.rs.QueryParam(value = 'user')) user:String
+	):String {
+		final realm = session.realms().getRealm(realm);
+		
+		// TODO: realm can be null
+		final user = session.users().getUserById(realm, user);
+		
+		// TODO: user can be null
+		user.setFederationLink(null);
+		
+		if (session.getTransactionManager().isActive()) {
+			session.getTransactionManager().commit();
+		} else {
+			throw 'Not in transaction, user cannot be updated';
+		}
+		
+		return haxe.Json.stringify({
+			result: 'ok'
 		});
 	}
 }
